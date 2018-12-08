3e8670b6d67889cd9389be9ebcf383546498feb9
Srikar Namburi <39098998+SrikarNamburi@users.noreply.github.com>
2018-12-07 19:44:08 -0800

    Create NetworkTables.h

---
+++ RoboMechanism/src/NetworkTablesInterface.h
@@ -0,0 +1,11 @@
+#ifndef SRC_UTILITIES_NETWORKTABLESINTERFACE_H_
+#define SRC_UTILITIES_NETWORKTABLESINTERFACE_H_
+
+class NetworkTablesInterface {
+public:
+	static double gety();
+	static bool gethi();
+	static int getx();
+};
+
+#endif /* SRC_UTILITIES_NETWORKTABLESINTERFACE_H_ */
